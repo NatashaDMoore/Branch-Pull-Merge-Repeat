a33f067cca29a2e8dc3469605ec97f04e17a8687
Brandon Davila <97662996+AlivadTheImpala@users.noreply.github.com>
2023-03-09 13:06:59 -0800

    Adds beginning of fieldset and labels

--- TempCoverter.php
+++ TempCoverter.php
@@ -13,6 +13,28 @@
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
         <h2>Temperature Converter</h2>
         
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <h2>Temperature Converter</h2>
+
+        <fieldset>
+            <label>Temperature to Convert</label>
+            <input type="number" name="temp_from" step="any" value="<?php if (isset($_POST['temp_from'])) echo htmlspecialchars($_POST['temp_from']); ?>" />
+
+            <label>Convert From:</label>
+            <select name="scale_from">
+
+                <option value="" NULL<?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == NULL) echo 'selected = "unselected"'; ?>> Select one</option>
+
+                <option value="f" <?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == 'f') echo 'selected = "selected"'; ?>> Fahrenheit</option>
+
+                <option value="c" <?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == 'c') echo 'selected = "selected"'; ?>> Celsius </option>
+
+                <option value="k" <?php if (isset($_POST['scale_from']) && $_POST['scale_from'] == 'k') echo 'selected = "selected"'; ?>> Kelvin </option>
+
+            </select>
+
+            <label>Convert To:</label>
+        
         
 </body>
 
