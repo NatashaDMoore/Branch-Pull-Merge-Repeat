65f837a4948b9e0679ed4d7f04ae4ae3377410b6
Natasha <89048769+NatashaDMoore@users.noreply.github.com>
2023-03-09 12:37:44 -0800

    Create TempCoverter.php
    Included head and body

---
+++ TempCoverter.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Temperature Converter</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+</head>
+
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+        <h2>Temperature Converter</h2>
+        
+        
+</body>
+
+</html>
